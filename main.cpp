d432cf9fc6f4dbc00e84b38d217edda958adc1b9
SimonFernandezWu <15696@ait.nsw.edu.au>
2025-05-28 09:15:12 +0000

    namspace

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int a = 0;
-    std::cout << "Input a Number";
-    std::cin >> a;
-    std::cout << "The number you entered is " << a << "\n";
+    cout << "Input a Number";
+    cin >> a;
+    cout << "The number you entered is " << a << "\n";
     return 0;
 }
